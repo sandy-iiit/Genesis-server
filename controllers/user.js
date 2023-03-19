c4aa97c36882c90a2c778c35764b08e1432f274e
Datta Sandeep Choragudi <dattasandeep000@gmail.com>
2023-03-19 23:35:46 +0530

    added my-queries,write query,transport form

--- controllers/user.js
+++ controllers/user.js
@@ -33,7 +33,29 @@ exports.getPasswordChange=(req,res,next)=>{
 const array2=[{name:'Term Insurance',Duration:2+'yrs',Installment:300,type:'motor'},{type:'life',name:'Term Insurance',Duration:5+'yrs',Installment:500},
     {type:'health',name:'Term Insurance',Duration:2+'yrs',Installment:300},{name:'Term Insurance',Duration:2+'yrs',Installment:300}
 ,{name:'Term Insurance',Duration:2+'yrs',Installment:300},{name:'Term Insurance',Duration:2+'yrs',Installment:30000},
-    {name:'Term Insurance',Duration:2+'yrs',Installment:30000},{name:'Term Insurance',Duration:2+'yrs',Installment:30000},{name:'Term Insurance',Duration:2+'yrs',Installment:30000}]
+    {name:'Term Insurance',Duration:2+'yrs',Installment:30000},{name:'Term Insurance',Duration:2+'yrs',Installment:30000},{name:'Term Insurance',Duration:2+'yrs',Installment:30000},
+    {name:'Term Insurance',Duration:2+'yrs',Installment:780000}]
 exports.getMyPolicies=(req,res)=>{
-    res.render('my-policies',{arr:array2})
+    res.render('my-policies',{arr:array2,str:'Explore Plan'})
+}
+exports.getCurrentPolicies=(req,res)=>{
+    res.render('my-policies',{arr:array2,str:'Pay Installments'})
+}
+exports.getPayment=(req,res)=>{
+    res.render('payment',{})
+}
+const arr3=[{Q:"Cant I Pay my installments collectively for my plan id of Motor32AP?",A:'Yes"Sed ut aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodiluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi conseq consequatur? Quis au perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"'},
+    {Q:"Cant I Pay my installments collectively for my plan id of Motor32AP?",A:'Yes'},{Q:"Cant I Pay my installments collectively for my plan id of Motor32AP?",A:'Yes'},
+    {Q:"Cant I Pay my installments collectively for my plan id of Motor32AP?",A:'Yes'},
+    {Q:"Cant I Pay my installments collectively for my plan id of Motor32AP?",A:'Yes'}]
+exports.getMyQueries=(req,res)=>{
+    res.render('my-queries',{arr:arr3})
+}
+
+exports.getWriteQuery=(req,res)=>{
+    res.render('write-query',{})
+}
+
+exports.getTransportForm=(req,res)=>{
+    res.render('transport-form',{})
 }
