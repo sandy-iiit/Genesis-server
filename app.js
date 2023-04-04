1b82006d9e28f3a3db57839268d8aa19ca320c23
sandy-iiit <95034962+sandy-iiit@users.noreply.github.com>
2023-04-04 17:27:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -14,46 +14,10 @@ const userRoutes=require('./routes/userRoutes')
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 app.use(userRoutes)
-app.get('/',(req,res)=>{
-    res.render('home')
+app.get('*',(req,res)=>{
+    res.render('404')
 })
 
-// const db_name=path.join(__dirname,'data','database.db')
-// const db=new sqlite3.Database(db_name,err =>{
-//     if(err){
-//         console.log(err)
-//     }
-//     console.log('Database created')
-// })
-
-
-const tablecreate=`CREATE TABLE IF NOT EXISTS prac(
-id integer primary key autoincrement,
-name varchar(60) not null,
-email varchar(60) not null);`
-
-const insertdata=` insert into prac(name,email) values('manohar2','manohar2@gmail.com')`
-//
-// db.run(insertdata,err=>{
-//     if(err){
-//         console.log(err)
-//     }
-//     else
-//     console.log('data created')
-// })
-//
-// app.get('/data',(req,res)=>{
-//     const command=`select name from prac where name='Sandy' order by id`
-//
-//     db.all(command,(err,data)=>{
-//         if(err){
-//             console.log(err)
-//         }
-//         else{
-//             res.send(data)
-//         }
-//     })
-// })
 const seq=require('./data/base')
 
 seq.sync().then((result)=>{
