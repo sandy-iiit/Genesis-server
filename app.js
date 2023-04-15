2d971cee99f6e97e0dec852addd00eb82e1441fa
sandy-iiit <dattasandeep000@gmail.com>
2023-04-16 00:14:15 +0530

    Updated

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const flash = require('connect-flash');
 const app=express()
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 require('dotenv').config()
