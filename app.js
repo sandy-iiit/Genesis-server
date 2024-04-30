e623fd19eb9a13e8cfc35a38662ed3c9794bde6b
sandy-iiit <dattasandeep000@gmail.com>
2024-04-30 15:26:19 +0530

    added vercel

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ const fs = require('fs');
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:3000','http://10.0.14.118:3000'],
+    origin: ['http://localhost:3000','http://10.0.14.118:3000','https://genesis-react-1.vercel.app'],
+    // origin: "*",
     credentials: true,
 }));
 
